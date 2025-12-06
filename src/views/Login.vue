d45c0f4695237be6be43fa71d159a917b0505c8f
“nameisjar” <nthia4@gmail.com>
2025-12-07 04:02:43 +0700

    change icon login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -47,9 +47,16 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                d="M3 6h18v12H3z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 6l9 7 9-7"
               />
             </svg>
+
             Email
           </span>
           <input
@@ -318,8 +325,8 @@ const login = async () => {
 }
 
 .field-icon {
-  width: 18px;
-  height: 18px;
+  width: 24px;
+  height: 24px;
   color: #667eea;
 }
 
